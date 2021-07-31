432489f33d362d3ecf515020cbb33ea4902cadbb
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-07-31 15:50:22 +0900

    Initial commit

---
+++ src/main/java/com/xylope/gitpolio_server/GitpolioServerApplication.java
@@ -0,0 +1,13 @@
+package com.xylope.gitpolio_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpolioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitpolioServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xylope/gitpolio_server/GitpolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xylope.gitpolio_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpolioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
